9ad7877b4fdad1d3370421926fe77e7c7c755324
Sasipasch <sasipas.ch@chococrm.com>
2023-06-24 12:39:25 +0700

    feat: get method

--- go-api.go
+++ go-api.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type album struct {
 	ID     string  `json:"id"`
@@ -16,8 +20,27 @@ var albums = []album{
 		Artist: "John Coltrane",
 		Price:  56.99,
 	},
+	{
+		ID:     "2",
+		Title:  "Jeru",
+		Artist: "Gerry Mulligen",
+		Price:  17.99,
+	},
+	{
+		ID:     "3",
+		Title:  "Sarah Vaughan and Clifford Brown",
+		Artist: "Sarah Vaughan",
+		Price:  39.99,
+	},
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
